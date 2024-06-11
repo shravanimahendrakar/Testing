c3ed64b98e7569d8dc2df45d44a2b128c10a2030
shravanimahendrakar <mahendrakarshravani@gmail.com>
2024-06-11 12:06:09 -0400

    Add project files.

---
+++ Ecommerace/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerace
+{
+    public class Product
+    {
+ 
+            //Attributes with its setter and getter
+            public int ProductID { get; set; }
+            public string ProductName { get; set; }
+            public double Price { get; set; }
+
+            public int Stock { get; set; }
+
+            //constructor of Product class
+            public Product(int productId, string productName, double price, int stock)
+            {
+                ProductID = productId;
+                ProductName = productName;
+                Price = price;
+                Stock = stock;
+            }
+
+            //Method for increase stock
+            public void IncreaseStock(int amt)
+            {
+                if (amt < 0)
+                    throw new ArgumentException("Amount to increase cannot be negative.");
+
+                Stock += amt;
+            }
+
+            //Method for decrease stock
+            public void DecreaseStock(int amt)
+            {
+                if (amt >= Stock)
+                    Stock -= amt;
+                else
+                    throw new ArgumentException("Insufficient stock to decrease.");
+            }
+
+        }
+    }
+
+
